cbaab12037123e1d5558c00965cddda3d8cd960c
German Jaber <germanjaber@gmail.com>
2012-07-13 11:14:15 -0430

    - Added some lines to the .gitignore - Included the Filtering library in fotochop.hs

--- fotochop.hs
+++ fotochop.hs
@@ -16,6 +16,7 @@ import Control.Concurrent.MVar
 import Control.Monad
 
 import DevILWrapper
+import Filter (noRed,defaultFilter,filterImage)
 
 -- Comand line data type
 data ImageCmd = Image { ifile :: String, ofile :: String }
@@ -28,7 +29,7 @@ image = Image {
   ofile = def
           &= help "The image to be written in disc after filtering."
           &= typ "OUTPUT_IMAGE_FILE"
-  } &= summary ("FotoChop v0.0, (c) 2012 Victor De Ponte & Germán Jaber, "++
+  } &= summary ("FotoChop v0.1, (c) 2012 Victor De Ponte & Germán Jaber, "++
         "(c) 2012 Universidad Simon Bolivar.")
 
 -- main functions
@@ -38,6 +39,7 @@ main = do
   [imgFile] <- getArgs
   ilInit
   originalPicture <- readImage' imgFile
+  --mutablePicture <- newMVar $ filterImage defaultFilter originalPicture
   mutablePicture <- newMVar originalPicture
   forkOS $ showImage originalPicture mutablePicture
   mainIOLoop
@@ -77,24 +79,27 @@ showImage img1 img2  =
     winHeight <- return $ max (w1+1) (w2+1)
     (progname,_) <- getArgsAndInitialize
     initialDisplayMode $= [Graphics.UI.GLUT.DoubleBuffered]
-    createWindow "Fotochop 0.1"
-    windowSize $= Size (fromIntegral winWidth :: Int32) (fromIntegral winHeight :: Int32)
+    createWindow "Fotochop v0.1"
+    windowSize $=
+      Size (fromIntegral winWidth :: Int32) (fromIntegral winHeight :: Int32)
     clearColor $= Color4 0 0 0 1
     clear [ColorBuffer]
     displayCallback $= (drawImages img1 img2 (0,0) (winWidth,winHeight))
     mainLoop
 
-drawPoint' :: (Int,Int) -> (Int,Int) -> ((Int,Int),(Word8,Word8,Word8,Word8)) -> IO ()
+drawPoint' :: (Int,Int) -> (Int,Int) ->
+              ((Int,Int),RGBAPixel) -> IO ()
 drawPoint' winSize imgSize (pos,color) = drawPoint winSize imgSize color pos
 
-drawPoint :: (Int,Int) -> (Int,Int) -> (Word8,Word8,Word8,Word8) -> (Int,Int) -> IO ()
-drawPoint (winWidth,winHeight) (imgWidth,imgHeight) (r,g,b,a) pos@(x,y) = do
-  color $ (Color4 r g b a :: Color4 Word8)
-  vertex $ Vertex3 xcoor ycoor 0.0
-    where iw2 = (int2Float imgWidth)/2.0
-          ih2 = (int2Float imgHeight)
-          xcoor = ((int2Float y)/ih2 - 1.0)
-          ycoor = ((int2Float x)/iw2 - 1.0)
+drawPoint :: (Int,Int) -> (Int,Int) -> RGBAPixel -> (Int,Int) -> IO ()
+drawPoint (winWidth,winHeight) (imgWidth,imgHeight) (r,g,b,a) pos@(x,y) =
+  do
+    color $ (Color4 r g b a :: Color4 Word8)
+    vertex $ Vertex3 xcoor ycoor 0.0
+      where iw2 = (int2Float imgWidth)/2.0
+            ih2 = (int2Float imgHeight)
+            xcoor = ((int2Float y)/ih2 - 1.0)
+            ycoor = ((int2Float x)/iw2 - 1.0)
 
 {- TO DO: resize window when peeking in the MVar -}
 drawImages :: Img ->
@@ -103,7 +108,9 @@ drawImages :: Img ->
 drawImages img1 img2 offset1 winSize@(winWidth,winHeight) =
   let (_ , (w1,h1)) = bounds img1
       imgSize1 = (w1,h1)
-      offset2 = (0,(fst offset1)+h1) --The offset is in the X direction, but the #$%& coordinate system of DevIL forces me to put it in the Y component
+      offset2 = (0,(fst offset1)+h1)
+      --The offset is in the X direction, but the #$%& coordinate system of
+      --DevIL forces me to put it in the Y component
       fstImg = renderPrimitive Points $
                F.mapM_ ((drawPoint' winSize imgSize1).(addOffset offset1))
                (assocs img1) in
@@ -122,8 +129,8 @@ drawImages img1 img2 offset1 winSize@(winWidth,winHeight) =
     flush
     swapBuffers
 
-addOffset :: (Int,Int) -> ((Int,Int),(Word8,Word8,Word8,Word8)) ->
-             ((Int,Int),(Word8,Word8,Word8,Word8))
+addOffset :: (Int,Int) -> ((Int,Int),RGBAPixel) ->
+             ((Int,Int),RGBAPixel)
 addOffset (offx,offy) ((x,y),color) = ((x+offx,y+offy),color)
 
 
@@ -132,7 +139,7 @@ addOffset (offx,offy) ((x,y),color) = ((x+offx,y+offy),color)
 testImage :: Int -> Int -> Img
 testImage w h = array ((0,0),(h-1,w-1)) (sampleImg1Bindings w h)
 
-sampleImg1Bindings :: Int -> Int -> [((Int,Int),(Word8,Word8,Word8,Word8))]
+sampleImg1Bindings :: Int -> Int -> [((Int,Int),RGBAPixel)]
 sampleImg1Bindings w h =
   [((x,y) , (c (x,y), 0, c (x,y),255)) |
    x<-[0..(h-1)] , y<-[0..(w-1)]]
